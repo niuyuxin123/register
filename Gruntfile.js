7fa0dd46e0e6d15cae3944d28deb842686bc473a
niuyuxin <1120733074@qq.com>
2020-03-18 16:01:49 +0800

    a

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,47 +1,35 @@
 
-module.exports = function(grunt) {
-    grunt.initConfig({
-
-        htmlmin: {
-            options: {
-                collapseWhitespace: true
-            },
-            files: {
-                src: "./index.html",
-                dest: "dist/index.html"
-            }
-        },
-        cssmin: {
-            "dist/layout.css": "./layout.css"
-        },
-        uglify: {
-            release: {
-                files: {
-                    "dist/main.js": "./main.js"
-                }
-            }
-        },
-        copy: { 
-            html: {
-                src: "./index.html", 
-                dest: "./dist/index.html"
-            },
-            css: {
-                src: "./layout.css",
-                dest: "./dist/layout.css"
-            },
-            js: {
-                src: "./main.js",
-                dest: "./dist/main.js"
-            }
+module.exports = function (grunt) {
+  grunt.initConfig({
+    htmlmin: {
+      options: {
+        collapseWhitespace: true
+      },
+      files: {
+        src: './index.html',
+        dest: 'dist/index.html'
+      }
+    },
+    cssmin: {
+      'dist/layout.css': 'layout.css'
+    },
+    uglify: {
+      release:{
+        files: {
+          'dist/main.js': 'main.js'
         }
-    })
+      }
+    }
+  });
 
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
-    grunt.loadNpmTasks("grunt-contrib-htmlmin");
-    grunt.loadNpmTasks("grunt-contrib-copy");
-    grunt.loadNpmTasks("grunt-contrib-cssmin");
-    grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-    grunt.registerTask("release", ['copy', 'htmlmin', 'cssmin', 'uglify']);
-} 
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks("grunt-contrib-copy");
+
+  grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify']);
+  grunt.registerTask("release", ['copy', 'htmlmin','cssmin', 'uglify']);
+};
+
